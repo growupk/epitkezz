7994c52dfcab3f1bc416a24a429beacef4116f82
BOBISPC\user <bobis.andras@gmail.com>
2017-10-28 16:52:11 +0200

    "Téglaház" page half complete

--- wp-content/themes/epitkezz/template-kivitelezes.php
+++ wp-content/themes/epitkezz/template-kivitelezes.php
@@ -8,7 +8,7 @@
         <h3><?= the_field('kivitelezes_cim');?></h3>
         <div class="row">
             <div class="col-lg-6 left-box" style="background-image: url('<?= the_field('kivitelezes_bal_oldali_doboz_kep');?>');">
-                <a href="#"><?= the_field('kivitelezes_bal_oldali_doboz_gomb_szovege');?></a>
+                <a href="<?= get_permalink( get_page_by_title( 'téglaház' ) );?>"><?= the_field('kivitelezes_bal_oldali_doboz_gomb_szovege');?></a>
             </div>
             <div class="col-lg-6 right-box" style="background-image: url('<?= the_field('kivitelezes_jobb_oldali_doboz_kep');?>');">
                 <a href="#"><?= the_field('kivitelezes_jobb_oldali_doboz_gomb_szovege');?></a>
---
+++ wp-content/themes/epitkezz/template-teglahaz.php
@@ -0,0 +1,46 @@
+<?php
+    /* Template Name: Téglaház */
+    get_header();
+?>
+
+<section class="brick-house-header" style="background-image: url('<?= the_field('teglahaz_fejlec_kep');?>');">
+    <div class="img-text">
+        <h3><?= the_field('teglahaz_fejlec_szoveg_01');?></h3>
+        <h4><?= the_field('teglahaz_fejlec_szoveg_02');?></h4>
+    </div>
+    <div class="red-bottom-border">
+        <div class="logo">
+            <img src="<?php the_field('teglahaz_fejlec_logo');?>" alt="logo teglaház" class="img-responsive">
+            <a href="<?= the_field('teglahaz_fejlec_jobb_also_link');?>"><?= the_field('teglahaz_fejlec_jobb_also_link');?></a>
+        </div>
+    </div>
+</section>
+<section class="brick-type">
+    <div class="container">
+        <h3><?= the_field('teglahaz_teglatipusok_cim');?></h3>
+        <div class="row">
+            <?php if( have_rows('teglahaz_tegla_tipusok') ): ?>
+                <?php while( have_rows('teglahaz_tegla_tipusok') ): the_row(); ?>
+                    <div class="col-xs-12 col-lg-4 brick">
+                        <h4><?= the_sub_field('teglahaz_tegla_tipus');?></h4>
+                        <img src="<?php the_sub_field('teglahaz_tegla_tipus_kep');?>" alt="tégla típus" class="img-responsive">
+                        <p class="sub-title"><?= the_sub_field('teglahaz_tegla_tipus_alcim');?></p>
+                        <span class="bottom-content"><?= the_sub_field('teglahaz_tegla_tipus_leiras');?></span>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+<section class="heating" style="background-image: url('<?= the_field('teglahaz_futes_rendszer_hatterkep');?>');">
+    <div class="overlay"></div>
+    <h3><?= the_field('teglahaz_futes_rendszer_cim');?></h3>
+    <div class="container">
+        <div class="row">
+            <?= the_field('teglahaz_futes_rendszer_szoveges_tartalom');?>
+            <a target="_blank" href="https://www.bvfheating.hu/futofilm-futofolia/?gclid=Cj0KEQjwuOHHBRDmvsHs8PukyIQBEiQAlEMW0KKc_1X2kjz_UyGB6GjXcC6dupmD_f5bYBfZ18qT6jUaAjwS8P8HAQ"><img src="<?php echo get_bloginfo('stylesheet_directory');?>/assets/img/galeo-color.png" alt="tégla típus" class="img-responsive"></a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
