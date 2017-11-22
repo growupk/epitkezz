f4a09898d49176139a62e554b1a070cd01e49d3f
bobis andras <bobis.andras@e-vista.hu>
2017-11-22 08:36:53 -0800

    References page complete -> media slider added, search plugin added

---
+++ wp-content/themes/epitkezz/template-references.php
@@ -0,0 +1,11 @@
+<?php
+    /* Template Name: Referenciák */
+    get_header();
+?>
+
+<section class="references">
+    <h2><?php echo get_the_title();?></h2>
+    <?php masterslider(1); ?>
+</section>
+
+<?php get_footer(); ?>
