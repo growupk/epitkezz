d93e9e695f96a5971ba7d4c27b57b27f41ffa2f2
bobis andras <bobis.andras@e-vista.hu>
2017-11-22 13:41:38 -0800

    Search complete

--- wp-content/themes/epitkezz/search.php
+++ wp-content/themes/epitkezz/search.php
@@ -4,31 +4,35 @@
 
 		<h2>Keresés eredménye:</h2>
 
-		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
+		<?php //include (TEMPLATEPATH . '/inc/nav.php' ); ?>
 
 		<?php while (have_posts()) : the_post(); ?>
-
+			<?php
+				$permalink = get_permalink( $post->ID );
+				if( $permalink && preg_match("/houses\/[a-z]*.\/?!*$/", $permalink, $matches)){
+			?>
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<h3><a href="<?php echo get_permalink( $post->ID ); ?>"><?php the_title(); ?> <?php _e('Tipusterv','epitkezz')?></a></h3>
 
-				<h2><?php the_title(); ?></h2>
+				<?php //include (TEMPLATEPATH . '/inc/meta.php' ); ?>
 
-				<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+				<!-- <div class="entry"> -->
 
-				<div class="entry">
+					<?php //the_excerpt(); ?>
 
-					<?php the_excerpt(); ?>
-
-				</div>
-
+				<!-- </div> -->
 			</div>
-
+			<?php
+				}
+			?>
 		<?php endwhile; ?>
 
 		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
 
 	<?php else : ?>
-
-		<h2>Nincs találat a megadott keresési feltételre</h2>
+		<div class="search-null">
+			<h2>Nincs találat a megadott keresési feltételre</h2>
+		</div>
 
 	<?php endif; ?>
 
